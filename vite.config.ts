c3f442292c025333d303021189e8ae2a5c0d6451
aabderhman201-commits <3.21021051e+08+aabderhman201-commits@users.noreply.github.com>
2026-09-08 20:31:30 +0000

    Add continuous GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/KING-DESIGNER/',
   plugins: [react()],
   resolve: {
     alias: {
